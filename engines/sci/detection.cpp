73056b4d5ebd14ebedd3db42f959febb5408e638
UrQuan1 <44381169+UrQuan1@users.noreply.github.com>
2018-11-01 13:06:56 +0200

    Update detection.cpp

--- engines/sci/detection.cpp
+++ engines/sci/detection.cpp
@@ -126,7 +126,7 @@ static const PlainGameDescriptor s_sciGameTitles[] = {
 	// === SCI3 games =========================================================
 	{"lsl7",            "Leisure Suit Larry: Love for Sail!"},
 	{"lighthouse",      "Lighthouse: The Dark Being"},
-	{"phantasmagoria2", "Phantasmagoria: A Puzzle of Flesh"},
+	{"phantasmagoria2", "Phantasmagoria 2: A Puzzle of Flesh"},
 	//{"shivers2",        "Shivers II: Harvest of Souls"},	// Not SCI
 	{"rama",            "RAMA"},
 	{0, 0}
